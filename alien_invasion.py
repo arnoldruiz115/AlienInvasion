d54b3b93e7806d756277823eb573ae0e1a287464
arnold.r115 <arnoldruiz115@csu.fullerton.edu>
2019-09-25 14:16:00 -0700

    Update

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,3 +1,24 @@
+import sys
 import pygame
+from settings import Settings
+from ship import Ship
 
-pygame.init()
+def run_game():
+    pygame.init()
+    game_settings = Settings()
+    screen = pygame.display.set_mode((game_settings.screen_width, game_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+    ship = Ship(screen)
+
+    # Main loop for the game
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        screen.fill(game_settings.bg_color)
+        ship.blitme()
+        pygame.display.flip()
+
+
+run_game()
---
+++ settings.py
@@ -0,0 +1,6 @@
+class Settings:
+    # A class to store all Settings
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (65, 66, 79)
